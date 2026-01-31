1f181613fdac47f65a61ab6710d5a1fde95e54ef
Nicofire <nesich.nicolas@gmail.com>
2026-01-31 21:27:56 +0100

    feat: add initial login credentials output to AMP setup script

--- ct/amp.sh
+++ ct/amp.sh
@@ -43,3 +43,8 @@ msg_ok "Completed successfully!\n"
 echo -e "${CREATING}${GN}${APP} setup has been successfully initialized!${CL}"
 echo -e "${INFO}${YW} Access it using the following URL:${CL}"
 echo -e "${TAB}${GATEWAY}${BGN}http://${IP}:8080${CL}"
+
+echo -e "\n${INFO}${YW} Initial Login Credentials:${CL}"
+echo -e "${TAB}${BGN}Username: admin${CL}"
+echo -e "${TAB}${BGN}Password: ProxmoxAMP2026!${CL}"
+echo -e "${TAB}${YW}Please change your password immediately after the first login!${CL}"
