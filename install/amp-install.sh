96ad119763db3589feaaae649ed15cff2c02f6cc
Nicofire <nesich.nicolas@gmail.com>
2026-01-31 21:11:33 +0100

    feat: update AMP installation script with configuration options

--- install/amp-install.sh
+++ install/amp-install.sh
@@ -18,6 +18,14 @@ $STD apt-get install -y curl sudo gnupg2 dirmngr
 msg_ok "Installed Dependencies"
 
 msg_info "Installing AMP (CubeCoders)"
+
+AMPUSER="admin" \
+AMPPASS="ProxmoxAMP2026!" \
+USE_ANSWERS=y \
+ANSWER_INSTALLJAVA=y \
+ANSWER_INSTALLSRCDSLIBS=y \
+ANSWER_INSTALLDOCKER=n \
+ANSWER_HTTPS=n \
 bash <(curl -fsSL https://getamp.sh)
 msg_ok "AMP (CubeCoders) installed"
 
